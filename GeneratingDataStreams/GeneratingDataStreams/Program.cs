b72f03f906d5df8a42ac6cdc36d8e2402c865660
gianvittorio <gianvittorio.castellano@gmail.com>
2022-11-09 15:06:00 -0300

    done with generating data streams

---
+++ GeneratingDataStreams/GeneratingDataStreams/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MasterLinq
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Generating Range:");
+            foreach (var num in Enumerable.Range(5, 8))
+            {
+                Console.WriteLine($"Num: {num}");
+            }
+
+            Console.WriteLine("Repeating:");
+            foreach (var num in Enumerable.Repeat(10, 5))
+            {
+                Console.WriteLine($"Num: {num}");
+            }
+            
+            Console.WriteLine("Randomizing");
+            foreach (var d in RandomStream.Generate().Where(num => num > .7f).Take(5))
+            {
+                Console.WriteLine($"Num: {d.ToString("F2")}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> GetData()
+        {
+            return Enumerable.Empty<int>();
+        }
+    }
+
+    public static class RandomStream
+    {
+        public static IEnumerable<double> Generate()
+        {
+            var random = new Random();
+            for (;;)
+            {
+                yield return random.NextDouble();
+            }
+        }
+    }
+}
